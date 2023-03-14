073d98c08ef8fcb8ff848214ffc5179932bb9150
TpentDev <hdesk@mod.mil.gr>
2023-03-14 10:54:56 +0200

    Create the basic header configuration

--- src/app/modules/core/components/navigation/header/header.component.ts
+++ src/app/modules/core/components/navigation/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Output() sidenavToggle = new EventEmitter<void>();
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
+
+  onLogout() {
+    
+  }
 }
